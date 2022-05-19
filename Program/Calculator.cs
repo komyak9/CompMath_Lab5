26eeba74506dad866d1a142525a32195bac4014b
komyak9 <komyak9@gmail.com>
2022-05-19 20:04:58 +0300

    Code clean up

--- Program/Calculator.cs
+++ Program/Calculator.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using SplineInterpolationLibrary;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SplineInterpolationLibrary;
 
 namespace Lab_5
 {
--- Program/Functions/FunctionTwo.cs
+++ Program/Functions/FunctionTwo.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using SplineInterpolationLibrary;
+﻿using SplineInterpolationLibrary;
+using System;
 
 namespace Lab_5
 {
